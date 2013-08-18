2492d8072e9a4dbc19fcf782c5e5d0f327fa3b48
Mark Claydon <developer@glia.co>
2013-08-18 13:22:20 +0100

    Moved $

--- scripts/bamboo-git-push.sh
+++ scripts/bamboo-git-push.sh
@@ -2,8 +2,8 @@
 
 echo "Git add, commit, push"
 
-$DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo DIR 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR 
 
 #git add -A
 #git commit -m "[bamboo] Build from website"
